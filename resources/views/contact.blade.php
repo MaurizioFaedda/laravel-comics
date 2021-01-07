32d6c1e7d807c526ab9d37f73a183b3fa5810782
Maurizio <maurizio.faedda1@gmail.com>
2021-01-07 14:38:52 +0100

    init repo and added top header

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>contact-us</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,24 @@
+<header>
+    <div class="header-top">
+        <div class="container">
+            <nav>
+                <ul>
+                    <li>
+                        <a class="uppercase" href="#">
+                            DC power℠ visa®
+                        </a>
+                    </li>
+                    <li>
+                        <a class="uppercase" href="#">
+                            additional dc sites
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+    </div>
+    <div class="header-bottom">
+
+    </div>
+</header>
