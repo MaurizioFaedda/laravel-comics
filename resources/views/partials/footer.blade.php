a486304f29990f2b647dbbaaf39b71d09bfd9a31
Maurizio <maurizio.faedda1@gmail.com>
2021-01-07 18:03:32 +0100

    added footer background

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,14 @@
 <footer>
-    <div class="footer-top">
+    <div class="container">
+        <div class="footer-top container">
+            <div class="footer-menu">
+            </div>
+            <div class="footer-right">
+            </div>
+        </div>
+        <div class="footer-bottom">
 
-    </div>
-    <div class="footer-bottom">
+        </div>
 
     </div>
 </footer>
